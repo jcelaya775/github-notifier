b228f764cfe335321104e77c9bc4a252e8744c98
Jorge Celaya <jcelaya775@gmail.com>
2025-09-13 22:55:34 -0500

    temp

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"embed"
 	_ "embed"
 	"fmt"
+	"github.com/wailsapp/wails/v3/pkg/services/badge"
 	"log"
 	"time"
 
@@ -18,10 +19,22 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
+//go:embed assets/github-light.png assets/github-dark.png
+var iconFS embed.FS
+
+const APP_TITLE = "GitHub Notifier"
+
+var windowIsVisible = false
+
 // main function serves as the application's entry point. It initializes the application, creates a window,
 // and starts a goroutine that emits a time-based event every second. It subsequently runs the application and
 // logs any error that might occur.
 func main() {
+	// System tray notification count badge
+	badgeService := badge.New()
+	if err := badgeService.SetBadge("3"); err != nil {
+		log.Fatal(err)
+	}
 
 	// Create a new Wails application by providing the necessary options.
 	// Variables 'Name' and 'Description' are for application metadata.
@@ -33,6 +46,7 @@ func main() {
 		Description: "A demo of using raw HTML & CSS",
 		Services: []application.Service{
 			application.NewService(&GreetService{}),
+			application.NewService(badgeService),
 		},
 		Assets: application.AssetOptions{
 			Handler: application.AssetFileServerFS(assets),
@@ -45,28 +59,35 @@ func main() {
 	systray := app.SystemTray.New()
 
 	// System tray icons
-	var iconFS embed.FS
-	lightModeIconBytes, _ := iconFS.ReadFile("assets/github-dark.png")
-	darkModeIconBytes, _ := iconFS.ReadFile("assets/github-light.png")
-
+	lightModeIconBytes, _ := iconFS.ReadFile("assets/github-light.png")
+	darkModeIconBytes, _ := iconFS.ReadFile("assets/github-dark.png")
 	systray.SetIcon(lightModeIconBytes)
 	systray.SetDarkModeIcon(darkModeIconBytes)
 
 	// System tray tooltip (Windows) and label (macOS)
-	systray.SetTooltip("GitHub Notifier")
-	systray.SetLabel("GitHub Notifier")
+	systray.SetTooltip(APP_TITLE)
+	systray.SetLabel(APP_TITLE)
 
 	// System tray menu
 	menu := application.NewMenu()
-	menu.Add("Open").OnClick(func(ctx *application.Context) {
-		// Handle click
-		fmt.Println("Open clicked")
-	})
 	menu.Add("Quit").OnClick(func(ctx *application.Context) {
 		app.Quit()
 	})
 
 	systray.SetMenu(menu)
+
+	// System tray window
+	window := app.Window.NewWithOptions(application.WebviewWindowOptions{
+		Title:  APP_TITLE,
+		Width:  200,
+		Height: 100,
+	})
+	systray.AttachWindow(window)
+
+	systray.OnClick(func() {
+		fmt.Println("Clicked on tray icon")
+		window.Show()
+	})
 
 	// Create a new window with the necessary options.
 	// 'Title' is the title of the window.
